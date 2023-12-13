29d6eb96dc7426fa0ac9ddb6e5deae9800fba734
Ikram Chouichi <ikram@netronian.com>
2023-12-13 10:20:22 -0500

    complete lab

--- index.js
+++ index.js
@@ -1,1 +1,14 @@
 // Code your solution in this file!
+
+// Declare and define the companyName variable
+const companyName = "Scuber";
+
+// Declare and define the mostProfitableNeighborhood variable
+let mostProfitableNeighborhood = "Chelsea";
+
+// Declare and define the companyCeo variable
+let companyCeo = "Susan Smith";
+
+
+
+
